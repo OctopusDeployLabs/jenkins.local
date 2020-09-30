c4a3fcb1d9dbbf43fe23484740eb53f8a7f8839d
Ryan Rousseau <ryan.rousseau@gmail.com>
2020-09-30 16:57:26 -0500

    Add logic to copy cacerts to jenkins_data

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+tag=$(get_octopusvariable "Project.Jenkins.ServerTag")
+
 echo "Stop jenkins-local."
 systemctl is-active --quiet jenkins-local && systemctl stop jenkins-local
 
@@ -19,8 +21,14 @@ if [ ! -d "jenkins_plugins" ]; then
     echo "jenkins_plugins/ configured."
 fi
 
+if [ ! f "jenkins_data/cacerts" ]; then
+    echo "Copy cacerts to jenkins_data"
+    docker run --volume "$(pwd)/jenkins_data:/bitnami/jenkins" \
+        bitnami/jenkins:$tag \
+        cp /opt/bitnami/java/jre/lib/security/cacerts /bitnami/jenkins/
+fi
+
 echo "Install plugins."
-tag=$(get_octopusvariable "Project.Jenkins.ServerTag")
 
 docker run --volume "$(pwd)/jenkins_plugins:/usr/share/jenkins/ref/plugins" \
     bitnami/jenkins:$tag \
